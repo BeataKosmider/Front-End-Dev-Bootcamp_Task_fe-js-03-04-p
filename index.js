bc62b265abd8ff5974174d4a6d2c1b2b58b6cc6b
Beata Kosmider <kosmibe@gmail.com>
2023-02-01 16:30:42 +0100

    task is done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-function filterItems() {
+function filterItems(arr1, arr2) {
+  const arrNew = arr1.concat(arr2);
+  const arrNewFiltered = arrNew.filter((item, _key) => item > 0);
+  return arrNewFiltered;
   // Zaimplementuj zadanie zgodnie z wytycznymi
-};
+}
 
 module.exports = filterItems;
